424c9d9529bc3e5c1d529b30ab26b82097470c85
miguelcecci <miguelceccineto@gmail.com>
2022-05-23 17:29:48 -0300

    fixed gradient optimization

--- app/GradTree.hs
+++ app/GradTree.hs
@@ -1,6 +1,6 @@
 module GradTree where
 
-data OperationType = Constant | Add | Multiply | Relu | Output deriving Show
+data OperationType = Input | Constant | Add | Multiply | Relu | Output | Diff deriving Show
 data GradTree = Value Float Float | Operation GradTree OperationType GradTree GradTree deriving Show
 
 -- Structure sketch
@@ -17,9 +17,24 @@ data GradTree = Value Float Float | Operation GradTree OperationType GradTree Gr
 --test values
 ta = newValue 5
 tb = newValue 2
-tc = newValue (-3)
-op1 = newOperation Add ta tb
+tc = newValue (3)
+op0 = newOperation Multiply tb tc
+op1 = newOperation Add op0 ta
 op2 = newOperation Relu op1 op1
+
+c1 = newConstant 4
+op3 = newOperation Diff op0 c1
+--op3 = setGrad (-1) op2
+--op4 = backward op3
+
+neuron = newOperation Diff op2 c1
+
+newInput id = newOperation Input va va
+  where va = Value id 0
+
+newConstant :: Float -> GradTree
+newConstant a = newOperation Constant va va
+  where va = Value a 0
 
 newValue :: Float -> GradTree
 newValue a = Value a 0
@@ -33,6 +48,17 @@ newOperation Multiply a b = Operation (Value (va*vb) 0) Multiply a b
         vb = getValue b
 newOperation Relu a b = Operation (Value (if va >= 0 then va else 0) 0) Relu a (Value 0 0)
   where va = getValue a
+newOperation Constant a b = Operation a Constant a (Value 0 0)
+newOperation Diff a b = Operation (Value (-mse) (-mse*0.57721)) Diff a b
+  where va = getValue a
+        vb = getValue b
+        mse = (va-vb)**2
+newOperation Input a b = Operation (Value 0 id) Input (Value 0 0) (Value 0 0)
+  where id = getValue a
+
+--setInput :: Int -> Float -> GradTree
+--setInput id value (Operation (Value _ id) Input a b) = Operation (Value value id) Input (setInput id value a) (setInput id value b)
+--setInput id value (Operation tree op a b) = Operation tree op (setInput id value a) (setInput id value b)
 
 getBoth :: GradTree -> GradTree 
 getBoth (Operation a _ _ _) = a
@@ -59,6 +85,15 @@ backward (Value x xg) = Value x xg
 backward (Operation out Add a b) = Operation out Add (backward $ (setGrad (backAdd out a) a)) (backward $ (setGrad (backAdd out b) b))
 backward (Operation out Multiply a b) = Operation out Multiply (backward $ (setGrad (backMultiply out b) a)) (backward $ (setGrad (backMultiply out a) b))
 backward (Operation out Relu a b) = Operation out Relu (backward $ (setGrad (backRelu out) a)) b
+backward (Operation out Constant a b) = Operation out Constant (backward $ (setGrad (backConstant out) a)) b
+backward (Operation out Input a b) = Operation out Input (backward $ (setGrad (backConstant out) a)) b
+backward (Operation out Diff a b) = Operation (setGrad (backMse out) out) Diff (backward $ (setGrad (backMse out) a)) (backward $ (setGrad (backMse out) b))
+
+backConstant :: GradTree -> Float
+backConstant out = 0
+
+backMse :: GradTree -> Float
+backMse out = -((getValue out)*0.57721)
 
 backAdd :: GradTree -> GradTree -> Float
 backAdd out value = (getValue value) + (getGrad out)
@@ -79,3 +114,8 @@ forward (Operation out op a b) = Operation (getBoth $ newOperation op ua ub) op 
         ub = forward b
 forward value = value
 
+optimizationPipeline :: Float -> GradTree -> GradTree
+optimizationPipeline learningRate tree = forward $ updateValues learningRate $ backward tree
+
+iterateOptimization :: GradTree -> Float -> Int -> GradTree
+iterateOptimization tree learningRate iterations = if iterations > 0 then (iterateOptimization (optimizationPipeline learningRate tree) learningRate (iterations-1)) else tree 
